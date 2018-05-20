8b1a46d70c5318b84e189b07c43d2a54828120c3
Tim Lauter <timlauter@0-0.nyc.rr.com>
2018-05-20 19:43:06 -0400

    second commit

---
+++ App.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import * as firebase from "firebase";
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+const config = {
+  apiKey: "AIzaSyCtIowvDCRTzXTiY14hGfrQ9oE_W-ze4Ss",
+  authDomain: "mire-bec3a.firebaseapp.com",
+  databaseURL: "https://mire-bec3a.firebaseio.com",
+  projectId: "mire-bec3a",
+  storageBucket: "mire-bec3a.appspot.com"
+};
+
+firebase.initializeApp(config);
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
